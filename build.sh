583b0a981e93cab1b940437440323cffdbf19da8
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2025-06-22 14:08:33 +0200

    Pterodactyl soft restart

--- build.sh
+++ build.sh
@@ -14,12 +14,12 @@ dotnet build --configuration $CONFIG
 # upload to pterodactyl server
 if [ $? == 0 ] && [ -n $PUSH ]; then
   scp ./$(basename $PWD)/bin/$CONFIG/net4.8/$(basename $PWD).dll "scp://$ENDPOINT_SSH/.config/SCP Secret Laboratory/LabAPI/plugins/7777"
-  curl "https://$ENDPOINT/api/client/servers/$SERVER_ID/power" \
+  curl "https://$ENDPOINT/api/client/servers/$SERVER_ID/command" \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer $TOKEN" \
   -X POST \
   -d '{
-        "signal": "restart"
+        "command": "softrestart"
       }'
 fi
